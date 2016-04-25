8754edf712c3985b496e9611df5df32029ea801d
Jan Machat <jan@redcley.com>
2016-04-25 15:11:56 -0700

    skipped files

---
+++ platforms/Azure/IoTLabWeather/SQL/Load Locations.sql
@@ -0,0 +1,57 @@
+-- ================================================================
+-- Author:		Jan Machat (Redcley LLC)
+-- Create date: 8 March 2016
+-- Description:	Loads the table of locations.
+-- Copyright © 2016 by Microsoft Corporation. All rights reserved.
+-- =================================================================
+INSERT
+	INTO Locations (
+		  Location
+		 ,LocationCode
+		 ,[State]
+		 )
+	VALUES
+		 ('Arlington Municipal','KAWO','WA') 
+		,('Bellingham, Bellingham International Airport','KBLI','WA') 
+		,('Bremerton, Bremerton National Airport','KPWT','WA') 
+		,('Burlington/Mount Vernon, Skagit Regional Airport','KBVS','WA') 
+		,('Chehalis-Centralia Airport','KCLS','WA') 
+		,('Deer Park, Deer Park Airport','KDEW','WA') 
+		,('Eastsound, Orcas Island Airport','KORS','WA') 
+		,('Ellensburg, Ellensburg Bowers Field','KELN','WA') 
+		,('Ephrata, Ephrata Municipal Airport','KEPH','WA') 
+		,('Everett Snohomish County','KPAE','WA') 
+		,('Fairchild Air Force Base','KSKA','WA') 
+		,('Fort Lewis / Gray U. S. Army Airfield','KGRF','WA') 
+		,('Friday Harbor, Friday Harbor Airport','KFHR','WA') 
+		,('Hoquiam, Bowerman Airport','KHQM','WA') 
+		,('Kelso, Kelso-Longview Airport','KKLS','WA') 
+		,('Moses Lake, Grant County Airport','KMWH','WA') 
+		,('Olympia, Olympia Airport','KOLM','WA') 
+		,('Omak, Omak Airport','KOMK','WA') 
+		,('Pasco, Tri-Cities Airport','KPSC','WA') 
+		,('Pearson Airfield','KVUO','WA') 
+		,('Pierce County-Thun Field','KPLU','WA') 
+		,('Port Angeles Coast Guard Air Station','KNOW','WA') 
+		,('Port Angeles Fairchild International Airport','KCLM','WA') 
+		,('Port Townsend Jefferson County International Airport','K0S9','WA') 
+		,('Pullman / Moscow Regional Airport','KPUW','WA') 
+		,('Quillayute, Quillayute State Airport','KUIL','WA') 
+		,('Range Op 13 - Yakima Training','K1QW','WA') 
+		,('Renton, Renton Municipal Airport','KRNT','WA') 
+		,('Richland AP AWOS','KRLD','WA') 
+		,('Seattle, Seattle Boeing Field','KBFI','WA') 
+		,('Seattle, Seattle-Tacoma International Airport','KSEA','WA') 
+		,('Shelton, Shelton Sanderson Field','KSHN','WA') 
+		,('Spokane, Felts Field','KSFF','WA') 
+		,('Spokane, Spokane International Airport','KGEG','WA') 
+		,('Stampede Pass','KSMP','WA') 
+		,('Sunnyside Municipal Airport','K1S5','WA') 
+		,('Tacoma / McChord Air Force Base','KTCM','WA') 
+		,('Tacoma, Tacoma Narrows Airport','KTIW','WA') 
+		,('The Dalles, Columbia Gorge Regional / The Dalles Municipal Airport','KDLS','WA') 
+		,('Walla Walla, Walla Walla Regional Airport','KALW','WA') 
+		,('Wenatchee, Pangborn Memorial Airport','KEAT','WA') 
+		,('Whidbey Island, Naval Air Station','KNUW','WA') 
+		,('Yakima, Yakima Air Terminal','KYKM','WA') 
+;
---
+++ platforms/Azure/IoTLabWeather/SQL/sp.PersistObservation.sql
@@ -0,0 +1,131 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- ================================================================
+-- Author:		Jan Machat (Redcley LLC)
+-- Create date: 12 March 2016
+-- Description:	Persists one observation for a particular location.
+-- Returns:     0 if succeeded, error code if failed.
+-- Copyright © 2016 by Microsoft Corporation. All rights reserved.
+-- =================================================================
+CREATE PROCEDURE dbo.PersistObservation
+	-- All parameters are passed as strings.
+	@locationCode      NVARCHAR(50),
+	@observedOn        NVARCHAR(50),
+	@wind              NVARCHAR(50),
+	@visibility        NVARCHAR(50),
+	@weather           NVARCHAR(50),
+	@skyConditions     NVARCHAR(50),
+	@temperatureAir    NVARCHAR(50),
+	@dewpoint          NVARCHAR(50),
+	@relativeHumidity  NVARCHAR(50),
+	@windChill         NVARCHAR(50),
+	@heatIndex         NVARCHAR(50),
+	@pressureAltimeter NVARCHAR(50),
+	@pressureSeaLevel  NVARCHAR(50),
+	@precipitation1hr  NVARCHAR(50),
+	@precipitation3hr  NVARCHAR(50),
+	@precipitation6hr  NVARCHAR(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	-- ASSUMPTION -------------------------------------------------------
+	-- NAs are passed as empty strings and all passed values are trimmed.
+	-- ------------------------------------------------------------------
+
+	-- Find whether we already have this observation.
+	DECLARE @observedDate DATETIME = CONVERT(DATETIME, @observedOn)
+	;
+	IF (SELECT 
+			COUNT (*)
+		FROM
+			Observations
+		WHERE
+			LocationCode = @locationCode
+			AND
+			ObservedOn = @observedDate )
+		> 0
+	BEGIN
+		SELECT 0;  -- not having to insert is a success
+		RETURN
+	END
+
+	-- We do not have this observation - we have to persist it.
+	-- Get the new ID.
+	DECLARE @id BIGINT = COALESCE(
+		(SELECT MAX(ObservationId) FROM Observations),
+		-1 ) + 1 -- this starts the sequence from 0
+	;
+
+	-- Relative humidity includes a percent sign which we do not want.
+	SET @relativeHumidity = REPLACE(@relativeHumidity, '%', '')
+	;
+
+	BEGIN TRY
+
+		-- First the observation.
+		INSERT INTO Observations
+			(ObservationId
+			,LocationCode
+			,ObservedOn
+			,Wind
+			,Visibility
+			,Weather
+			,TemperatureAir
+			,Dewpoint
+			,RelativeHumidity
+			,WindChill
+			,HeatIndex
+			,PressureAltimeter
+			,PressureSeaLevel
+			,Precipitation1hr
+			,Precipitation3hr
+			,Precipitation6hr)
+		VALUES
+			(@id
+			,@locationCode
+			,@observedOn
+			,@wind
+			,IIF(LEN(@visibility) = 0,        NULL, @visibility)
+			,@weather
+			,IIF(LEN(@temperatureAir) = 0,    NULL, @temperatureAir)
+			,IIF(LEN(@dewpoint) = 0,          NULL, @dewpoint)
+			,IIF(LEN(@relativeHumidity) = 0,  NULL, @relativeHumidity)
+			,IIF(LEN(@windChill) = 0,         NULL, @windChill)
+			,IIF(LEN(@heatIndex) = 0,         NULL, @heatIndex)
+			,IIF(LEN(@pressureAltimeter) = 0, NULL, @pressureAltimeter)
+			,IIF(LEN(@pressureSeaLevel) = 0,  NULL, @pressureSeaLevel)
+			,IIF(LEN(@precipitation1hr) = 0,  NULL, @precipitation1hr)
+			,IIF(LEN(@precipitation3hr) = 0,  NULL, @precipitation3hr )
+			,IIF(LEN(@precipitation6hr) = 0,  NULL, @precipitation6hr) )
+		;
+		-- Now the conditions.
+		WHILE LEN(@skyConditions) > 0
+		BEGIN
+
+			-- We know that conditions are 6 characters long and are
+			-- separated by spaces.
+			INSERT INTO SkyConditions
+				(ObservationId
+				,SkyCondition)
+			VALUES
+				(@id
+				,SUBSTRING(@skyConditions, 1, 6))
+			;
+			SET @skyConditions = SUBSTRING(@skyConditions, 8, LEN(@skyConditions) - 7)
+			;
+		END
+
+		SELECT 0
+	END TRY
+	BEGIN CATCH
+
+		SELECT @@ERROR
+	
+	END CATCH
+END
+GO
