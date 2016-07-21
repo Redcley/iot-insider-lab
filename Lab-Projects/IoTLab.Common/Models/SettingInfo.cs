324c848bffc0a477248e9affea3e2e8b61fa0522
David Ferreira <dave@davetheplatypus.com>
2016-07-21 11:59:23 -0700

    Delete SettingInfo.cs

--- Lab-Projects/IoTLab.Common/Models/SettingInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IoTLab.Common.Models
-{
-	public class SettingInfo
-	{
-		public string keyName { get; set; }
-		public string keyType { get; set; }
-		public string keyValue { get; set; }
-	}
-}
