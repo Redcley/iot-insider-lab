1a86e90db3a6dc65b5c88c1a7641d50874eef101
David Ferreira <dave@davetheplatypus.com>
2016-07-21 11:55:24 -0700

    Add files via upload

---
+++ Lab-Projects/IoTLab.AdDisplay/Models/IndividualFace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IoTLab.AdDisplay.Constants;
+
+namespace IoTLab.AdDisplay.Models
+{
+    public class IndividualFace
+    {
+        public int Age { get; set; }
+        public GenderEnum Gender { get; set; }
+        public Dictionary<string, float> Emotions { get; set; } = new Dictionary<string, float>();
+    }
+}
---
+++ Lab-Projects/IoTLab.Common/Helpers/OxfordAPIHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using IoTLab.Common.Constants;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace IoTLab.Common.Helpers
+{
+    /// <summary>
+    /// Allows easy access to oxford functions such as adding a visitor to whitelist and checing to see if a visitor is on the whitelist
+    /// </summary>
+    internal static class OxfordAPIHelper
+    {
+        // The Oxford Emotion API client
+        private static readonly EmotionServiceClient emotionApiClient = new EmotionServiceClient(GeneralConstants.OxfordEmotionAPIKey);
+
+        // The Oxford Face API client
+        private static readonly IFaceServiceClient faceApiClient = new FaceServiceClient(GeneralConstants.OxfordFaceAPIKey);
+
+        public static async Task<bool> DetectFaces(Stream ImageStream)
+        {
+            // Set up the facial attributes we want returned
+            var requiredFaceAttributes = new FaceAttributeType[]
+            {
+                FaceAttributeType.Age, 
+                FaceAttributeType.Gender
+            };
+
+            // Convert the inbound stream to a bitmap so we can slice off faces later for the emotion API
+
+            // Process the inbound image via the Face API
+            var faces = await faceApiClient.DetectAsync(ImageStream, returnFaceLandmarks: true, returnFaceAttributes: requiredFaceAttributes);
+            if (!faces.Any())
+            {
+                return false;
+            }
+
+            // Now process via the Emotion API
+            var emotions = await emotionApiClient.RecognizeAsync(ImageStream);
+            foreach (Emotion emotion in emotions)
+            {
+            }
+
+            // We have faces, so process the return values
+            foreach (Face face in faces)
+            {
+            }
+
+            // And return the result
+            return true;
+        }
+    }
+}
