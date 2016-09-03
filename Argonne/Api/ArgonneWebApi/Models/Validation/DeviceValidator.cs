3b28e30a3228702f21fb4084a010984c2f575156
Nathan Bell <nbell@nfusionsolutions.com>
2016-09-03 09:25:27 -0700

    Ad controller & validator

--- Argonne/Api/ArgonneWebApi/Models/Validation/DeviceValidator.cs
+++ Argonne/Api/ArgonneWebApi/Models/Validation/DeviceValidator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ArgonneWebApi.Models.Dto;
+﻿using ArgonneWebApi.Models.Dto;
 using FluentValidation;
 
 namespace ArgonneWebApi.Models.Validation
