3bbc106dc58ed657d7a3e82580cd0420ab851199
Nathan Bell <v-natbel@microsoft.com>
2016-09-20 15:49:47 -0700

    change analytics job input and output source names

--- Hanford/Cloud/StreamAnalytics/Query.sql
+++ Hanford/Cloud/StreamAnalytics/Query.sql
@@ -5,7 +5,7 @@ WITH averages AS
         AVG(temperature) as avgtemp,
         CONCAT(IoTHub.ConnectionDeviceId, CAST(System.TimeStamp as nvarchar(max))) as uid,
         System.TimeStamp AS Time
-    FROM iotlabdemo
+    FROM sensordata
     GROUP BY SlidingWindow(mi, 5), IoTHub.ConnectionDeviceId
 ),
 
@@ -14,36 +14,36 @@ PreviousEvent AS
     SELECT
 		IoTHub.ConnectionDeviceId as deviceId,
 		LAST(temperature) OVER(LIMIT DURATION(ss, 11)) as temperature
-    FROM iotlabdemo
+    FROM sensordata
 )
 
 SELECT
     averages.deviceId as deviceId,
 	averages.time,
 	averages.avgtemp,
-	iotlabdemo.temperature as TriggerTemp,
+	sensordata.temperature as TriggerTemp,
 	CONCAT(averages.deviceId, CAST(System.TimeStamp as nvarchar(max))) as uid,
 	CASE
-		WHEN ((iotlabdemo.temperature - averages.avgtemp) > 5) AND ((PreviousEvent.temperature  - averages.avgtemp) <= 5) THEN 'red'
-		WHEN ((iotlabdemo.temperature - averages.avgtemp) > 3) AND ((PreviousEvent.temperature - averages.avgtemp) <= 3) THEN 'green'
-		WHEN ((iotlabdemo.temperature - averages.avgtemp) > 1) AND ((PreviousEvent.temperature - averages.avgtemp) <= 1) THEN 'blue'
-		WHEN ((iotlabdemo.temperature - averages.avgtemp) < 5) AND ((PreviousEvent.temperature  - averages.avgtemp) >= 5) THEN 'green'
-		WHEN ((iotlabdemo.temperature - averages.avgtemp) < 3) AND ((PreviousEvent.temperature - averages.avgtemp) >= 3) THEN 'blue'	
+		WHEN ((sensordata.temperature - averages.avgtemp) > 5) AND ((PreviousEvent.temperature  - averages.avgtemp) <= 5) THEN 'red'
+		WHEN ((sensordata.temperature - averages.avgtemp) > 3) AND ((PreviousEvent.temperature - averages.avgtemp) <= 3) THEN 'green'
+		WHEN ((sensordata.temperature - averages.avgtemp) > 1) AND ((PreviousEvent.temperature - averages.avgtemp) <= 1) THEN 'blue'
+		WHEN ((sensordata.temperature - averages.avgtemp) < 5) AND ((PreviousEvent.temperature  - averages.avgtemp) >= 5) THEN 'green'
+		WHEN ((sensordata.temperature - averages.avgtemp) < 3) AND ((PreviousEvent.temperature - averages.avgtemp) >= 3) THEN 'blue'	
 		ELSE 'off'
 	END as Color,
 	CASE
-		WHEN (iotlabdemo.temperature - averages.avgtemp > 1) THEN 'true'
+		WHEN (sensordata.temperature - averages.avgtemp > 1) THEN 'true'
 		ELSE 'false'
 	END as Power
 INTO
-    hotpath
+    alerts
 FROM
-    iotlabdemo
+    sensordata
 JOIN PreviousEvent 
-    ON DATEDIFF(ss, iotlabdemo, PreviousEvent) between 0 and 6
-        AND iotlabdemo.IoTHub.ConnectionDeviceId = PreviousEvent.deviceId
+    ON DATEDIFF(ss, sensordata, PreviousEvent) between 0 and 6
+        AND sensordata.IoTHub.ConnectionDeviceId = PreviousEvent.deviceId
 JOIN averages 
-    ON DATEDIFF(ms, iotlabdemo, averages) = 0 
-    AND iotlabdemo.IoTHub.ConnectionDeviceId = averages.deviceId 
+    ON DATEDIFF(ms, sensordata, averages) = 0 
+    AND sensordata.IoTHub.ConnectionDeviceId = averages.deviceId 
 WHERE
-	(iotlabdemo.temperature - averages.avgtemp > 1 ) OR ((PreviousEvent.temperature - averages.avgtemp) > 1)
+	(sensordata.temperature - averages.avgtemp > 1 ) OR ((PreviousEvent.temperature - averages.avgtemp) > 1)
