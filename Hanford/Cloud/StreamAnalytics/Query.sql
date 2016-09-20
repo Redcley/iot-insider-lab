e0ef7e3eaa80703308bb16876e62d88f90276656
Nathan Bell <v-natbel@microsoft.com>
2016-09-20 14:36:43 -0700

    add more complex logic for led color and power to query

--- Hanford/Cloud/StreamAnalytics/Query.sql
+++ Hanford/Cloud/StreamAnalytics/Query.sql
@@ -7,6 +7,14 @@ WITH averages AS
         System.TimeStamp AS Time
     FROM iotlabdemo
     GROUP BY SlidingWindow(mi, 5), IoTHub.ConnectionDeviceId
+),
+
+PreviousEvent AS
+(
+    SELECT
+		IoTHub.ConnectionDeviceId as deviceId,
+		LAST(temperature) OVER(LIMIT DURATION(ss, 60)) as temperature
+    FROM iotlabdemo
 )
 
 SELECT
@@ -14,13 +22,28 @@ SELECT
 	averages.time,
 	averages.avgtemp,
 	iotlabdemo.temperature as TriggerTemp,
-	CONCAT(averages.deviceId, CAST(System.TimeStamp as nvarchar(max))) as uid
+	CONCAT(averages.deviceId, CAST(System.TimeStamp as nvarchar(max))) as uid,
+	CASE
+		WHEN ((iotlabdemo.temperature - averages.avgtemp) > 5) AND ((PreviousEvent.temperature  - averages.avgtemp) <= 5) THEN 'red'
+		WHEN ((iotlabdemo.temperature - averages.avgtemp) > 3) AND ((PreviousEvent.temperature - averages.avgtemp) <= 3) THEN 'green'
+		WHEN ((iotlabdemo.temperature - averages.avgtemp) > 1) AND ((PreviousEvent.temperature - averages.avgtemp) <= 1) THEN 'blue'
+		WHEN ((iotlabdemo.temperature - averages.avgtemp) < 5) AND ((PreviousEvent.temperature  - averages.avgtemp) >= 5) THEN 'green'
+		WHEN ((iotlabdemo.temperature - averages.avgtemp) < 3) AND ((PreviousEvent.temperature - averages.avgtemp) >= 3) THEN 'blue'	
+		ELSE 'off'
+	END as Color,
+	CASE
+		WHEN (iotlabdemo.temperature - averages.avgtemp > 1) THEN 'true'
+		ELSE 'false'
+	END as Power
 INTO
     hotpath
 FROM
     iotlabdemo
-JOIN
-    averages on iotlabdemo.IoTHub.ConnectionDeviceId = averages.deviceId
-    AND DATEDIFF(ms, iotlabdemo, averages) = 0
+JOIN PreviousEvent 
+    ON DATEDIFF(ss, iotlabdemo, PreviousEvent) between 0 and 61
+        AND iotlabdemo.IoTHub.ConnectionDeviceId = PreviousEvent.deviceId
+JOIN averages 
+    ON DATEDIFF(ms, iotlabdemo, averages) = 0 
+    AND iotlabdemo.IoTHub.ConnectionDeviceId = averages.deviceId 
 WHERE
-	ABS(iotlabdemo.temperature - averages.avgtemp) > 1 
+	(iotlabdemo.temperature - averages.avgtemp > 1 ) OR ((PreviousEvent.temperature - averages.avgtemp) > 1)
