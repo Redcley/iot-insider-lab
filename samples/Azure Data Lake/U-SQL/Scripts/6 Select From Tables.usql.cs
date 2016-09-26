b2369ab84e3efda0375ff38261305dcef11b5a7d
Jan Machat <jan@redcley.com>
2016-09-26 12:33:05 -0700

    Fooling with Data Lake.

---
+++ samples/Azure Data Lake/U-SQL/Scripts/6 Select From Tables.usql.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace U_SQL
+{
+    public static class Helpers
+    {
+        // We use this method in the U-SQL code page.
+        public static string Reverse(string s)
+        {
+            // This could be done in a single statement...
+            var charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
